32cdd442add75bcee78dbedb53aa7d2971b8c6b0
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-23 19:26:48 +1030

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello user!";
+            int playerScore = 0;
+
+
+
+            Console.WriteLine(greeting);
+            Console.WriteLine("Score: " + playerScore);
+
+            playerScore++;
+
+            Console.WriteLine("Now the score is " + playerScore);
+
+        }
+    }
+}
